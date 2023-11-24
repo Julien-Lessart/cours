6567ed9bf62f1342c3212175e86981e0755ac532
Julien-Lessart <julien.lessart@outlook.fr>
2023-11-24 15:41:50 +0100

    fix:level3

--- TP JS/level 3/ex1.js
+++ TP JS/level 3/ex1.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /*  
     Exercice 1 : Fizz Buzz
     Demandez à l'utilisateur de saisir un nombre "N". 
@@ -43,50 +42,4 @@ const remplacerMultiple = (nombre) => {
     }
 }
 
-=======
-/*  
-    Exercice 1 : Fizz Buzz
-    Demandez à l'utilisateur de saisir un nombre "N". 
-    Afficher tous les nombres allant de 0 à "N", avec les particularités suivantes :
-        Remplacez tous les multiples de 3 par "FIZZ"​
-        Remplacez tous les multiples de 5 par "BUZZ"​
-        Remplacez tous les multiples de 3 ET 5 par "FIZZBUZZ"
-        Exemple :
-            FIZZBUZZ​
-            1   ​
-            2   ​   
-            FIZZ​
-            4   ​
-            BUZZ​
-            FIZZ
-    Julien LESSART
-    22/11/2023
-*/
-
-/**
- * 
- * @param {number} nombre valeur max jusqu'où tester ses multiples
- * @throws {Error} Si la valeur est négative
- */
-const remplacerMultiple = (nombre) => {
-    let concatenation = '';
-    if(Math.sign(nombre) === -1) throw new Error(`L'entrée est négative`);
-    if(typeof(nombre) !== 'number') throw new Error(`L'entrée n'est pas un nombre`);
-
-    for(let i = 0; i <= nombre; i++){
-
-        // Si i est un multiple de 3
-        if((i % 3) === 0) concatenation = 'FIZZ';
-        // Si i est un multiple de 5
-        if((i % 5) === 0) concatenation = concatenation.concat('BUZZ');
-        // Sinon on affiche i
-        if(((i % 5) !== 0) && ((i % 3) !== 0)) concatenation = i;
-        console.log(concatenation);
-
-        // On remet la concatenation a vide pour la prochaine ligne
-        concatenation = '';
-    }
-}
-
->>>>>>> 89b878d90dcfa455f1bcd34b6e569c6246243e89
 remplacerMultiple(6);
--- TP JS/level 3/ex6.js
+++ TP JS/level 3/ex6.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /*  
     Exercice 1 : Qui est le plus grand ?
     Écrivez un programme qui demande à l'utilisateur de saisir 3 nombres puis trouver le plus grand.
@@ -6,13 +5,4 @@
         [12 , 54 , 32 ] --> 54
     Julien LESSART
     22/11/2023
-=======
-/*  
-    Exercice 1 : Qui est le plus grand ?
-    Écrivez un programme qui demande à l'utilisateur de saisir 3 nombres puis trouver le plus grand.
-    Résultat attendu dans la console :
-        [12 , 54 , 32 ] --> 54
-    Julien LESSART
-    22/11/2023
->>>>>>> 89b878d90dcfa455f1bcd34b6e569c6246243e89
 */
